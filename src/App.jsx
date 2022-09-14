c077a8804cdf04aabe37f53f88a10f51602936c1
yannick2009 <yannickkouakou13@gmail.com>
2022-09-14 13:49:41 +0000

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.scss";
+// import "./_responsive.scss";
 import BigTitle from "./components/BigTitle";
 import Generator from "./components/Generator";
 
